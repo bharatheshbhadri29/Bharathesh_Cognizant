09b4c1d85e013edc7860df5ca035afbcbbba0c69
bharatheshbhadri29 <bharatheshbhadri29@gmail.com>
2021-03-13 16:27:15 +0530

    first commit

---
+++ Demo/Demo.java
@@ -0,0 +1,11 @@
+/**
+*Name : L BHARATHESH
+*Description : Demo of Compilation and Running of Java Program
+*Date : 3/12/2021
+*/
+
+class First {
+    public static void main(String[] args) {
+        System.out.println("Hello From Java"); 
+    }
+}
